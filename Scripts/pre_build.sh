46a99f4a6a408b6a273675a93080b0e12c4e36ba
Peter-Schorn <petervschorn@gmail.com>
2021-01-13 22:10:35 -0600

    the pre-build script sources ~/.zshrc instead of a specific file.

--- Scripts/pre_build.sh
+++ Scripts/pre_build.sh
@@ -1,7 +1,7 @@
 cd $SRCROOT
 cd SpotifyMenuBar/Model
 
-source /Users/pschorn/.local/swift_testing_environment.sh
+source ~/.zshrc
 
 sed -r -i '' "s/(let __clientId__ = )\".*\"/\1\"$SPOTIFY_SWIFT_TESTING_CLIENT_ID\"/" 'Spotify.swift'
 sed -r -i '' "s/(let __clientSecret__ = )\".*\"/\1\"$SPOTIFY_SWIFT_TESTING_CLIENT_SECRET\"/" 'Spotify.swift'
